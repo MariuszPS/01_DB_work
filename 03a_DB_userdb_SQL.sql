69fd70de44ca475d3bf6555570759a012902739a
MariuszPS <k8kpol@gmail.com>
2022-05-04 08:29:11 +0000

    ok

---
+++ 03a_DB_userdb_SQL.sql
@@ -0,0 +1,16 @@
+/* USER TAB. Version 1 */
+
+/* Table users */
+DROP TABLE IF EXISTS boo.users;
+
+CREATE TABLE IF NOT EXISTS boo.users
+(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    userName VARCHAR(20) NOT NULL,
+    famillyName VARCHAR(20) NOT NULL,
+    firstName VARCHAR(20) NOT NULL
+);
+
+/* Struktur */
+DESCRIBE boo.users;
+
