b7c75b28b5e8503888098949d24f118588667b15
MariuszPS <k8kpol@gmail.com>
2022-05-05 10:30:09 +0000

    ok

--- 05_SQL_SELECT/05_3_que_sort.sql
+++ 05_SQL_SELECT/05_3_que_sort.sql
@@ -12,10 +12,12 @@ FROM stocks.ccc
 #ORDER BY ticker ASC -- SYM, alphabetisch, aufsteigend
 #ORDER BY ticker DESC -- SYM, alphabetisch, absteigend
 
-ORDER BY price ASC -- Kurs, numerisch, beginnend mit der günstigsten Aktie
+#ORDER BY price ASC -- Kurs, numerisch, beginnend mit der günstigsten Aktie
 #ORDER BY price DESC -- Kurs, numerisch, beginnend mit der teuerste Aktie
 #ORDER BY no_yrs DESC -- numerisch, Aktien mit der längsten Div.-Historie
 
+#ORDER BY sector DESC,industry ASC -- Kombination ASC / DESC alphabet.
+ORDER BY no_yrs DESC, sector ASC -- Kombination ASC / DESC alphabet. / numerisch
 
 -- Begrenzung
 LIMIT 20 -- X Zeilen ab 0
